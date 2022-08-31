bbac418ae2fe8eea0fabdf69b093753017948e4a
yuriy0803 <yuriy0803@gmail.com>
2022-08-31 18:53:42 +0200

    redis: support unix domain socket

--- storage/redis.go
+++ storage/redis.go
@@ -156,12 +156,16 @@ type Worker struct {
 }
 
 func NewRedisClient(cfg *Config, prefix string, pplns int64, CoinName string) *RedisClient {
-	client := redis.NewClient(&redis.Options{
+	options := redis.Options{
 		Addr:     cfg.Endpoint,
 		Password: cfg.Password,
 		DB:       cfg.Database,
 		PoolSize: cfg.PoolSize,
-	})
+	}
+	if cfg.Endpoint[0:1] == "/" {
+		options.Network = "unix"
+	}
+	client := redis.NewClient(&options)
 	return &RedisClient{client: client, prefix: prefix, pplns: pplns, CoinName: CoinName}
 }
 
