54dac9db73cc9a9c8fb426e7a1b33a219ef98d43
yuriy0803 <yuriy0803@gmail.com>
2022-09-02 18:01:15 +0200

    Per miner payout treshold

--- storage/redis.go
+++ storage/redis.go
@@ -758,6 +758,16 @@ func (r *RedisClient) GetBalance(login string) (int64, error) {
 	return cmd.Int64()
 }
 
+func (r *RedisClient) GetTreshold(login string) (int64, error) {
+	cmd := r.client.HGet(r.formatKey("miners", login), "payouttreshold")
+	if cmd.Err() == redis.Nil {
+		return 0, nil
+	} else if cmd.Err() != nil {
+		return 0, cmd.Err()
+	}
+	return cmd.Int64()
+}
+
 func (r *RedisClient) LockPayouts(login string, amount int64) error {
 	key := r.formatKey("payments", "lock")
 	result := r.client.SetNX(key, join(login, amount), 0).Val()
--- www/app/helpers/format-hashrate.js
+++ www/app/helpers/format-hashrate.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function formatHashrate(params/*, hash*/) {
   var hashrate = params[0];
   var i = 0;
-  var units = ['H', 'KH', 'MH', 'GH', 'TH', 'PH'];
+  var units = ['H/s', 'KH/s', 'MH/s', 'GH/s', 'TH/s', 'PH/s'];
   while (hashrate > 1000) {
     hashrate = hashrate / 1000;
     i++;
