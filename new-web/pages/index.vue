f3d0fe70b38ac6807bb60fcab95aea6d18443e07
yuriy0803 <68668177+yuriy0803@users.noreply.github.com>
2022-09-07 20:55:31 +0200

    Update index.vue

--- new-web/pages/index.vue
+++ new-web/pages/index.vue
@@ -70,8 +70,8 @@
             <li>{{ $tc('pages.home.mode', 0, { mode: config.mode }) }}</li>
             <li>
               {{ $t('pages.home.poweredBy') }}
-              <a href="https://github.com/etclabscore/core-pool" target="_blank"
-                >core-pool</a
+              <a href="https://github.com/yuriy0803/open-etc-pool-friends" target="_blank"
+                >open-etc-pool-friends</a
               >.
             </li>
             <li>{{ $t('pages.home.protocols') }}</li>
