67cf746c2d763b7472a3b91c1b29e275bf23e58a
yuriy0803 <68668177+yuriy0803@users.noreply.github.com>
2023-04-19 17:29:14 +0200

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -51,11 +51,6 @@ func (s *ProxyServer) handleLoginRPC(cs *Session, params []string, id string) (b
 		return false, &ErrorReply{Code: -1, Message: "You are blacklisted"}
 	}
 
-	// If password is provided, write it to the backend
-	if len(params) > 1 {
-		s.backend.WritePasswordByMiner(login, params[1])
-	}
-
 	// Update session information and register the session
 	cs.login = login
 	cs.worker = id
