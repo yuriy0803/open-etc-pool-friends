6eca90d53184792e106f85d6324f2f44f97bfa82
yuriy0803 <68668177+yuriy0803@users.noreply.github.com>
2023-05-12 13:01:22 +0200

    support geth --miner.notify option to push mining jobs
    ./geth --miner.notify http://127.0.0.1:8888/etc
    
    api.json
    
    https://github.com/yuriy0803/open-etc-pool-friends/blob/master/api.json#L12

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -12,7 +12,7 @@ import (
 	"github.com/yuriy0803/open-etc-pool-friends/util"
 )
 
-const maxBacklog = 3
+const maxBacklog = 10
 
 type heightDiffPair struct {
 	diff   *big.Int
@@ -59,9 +59,13 @@ func (s *ProxyServer) fetchBlockTemplate() {
 		return
 	}
 	// No need to update, we have fresh job
-	if t != nil && t.Header == reply[0] {
-		return
-	}
+	if t != nil {
+		if t.Header == reply[0] {
+			return
+		}
+		if _, ok := t.headers[reply[0]]; ok {
+			return
+		}
 
 	pendingReply.Difficulty = util.ToHex(s.config.Proxy.Difficulty)
 
