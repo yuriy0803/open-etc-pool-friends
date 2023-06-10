3529b8d5c0cd0a5bb6916ae83a59e3a14fbd84c0
yuriy0803 <68668177+yuriy0803@users.noreply.github.com>
2023-06-10 19:43:46 +0200

    cmd sed -i -e 's/\r$//' update.sh
    cmd
    sed -i -e 's/\r$//' update.sh

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# This is the Server install script.
+echo "open-etc-pool-friends update"
+echo "Please do NOT run as root, run as the pool user!"
+echo "If it not exist, create the pool user!"
+
+sleep 2
+
+echo "Update... Please wait!"
+
+
+
+
+git pull
+
+
+echo ""
+echo "Update complete!"
+echo ""
+
+exit 0
